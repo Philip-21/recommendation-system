a6d2ba36c6abeadb81af7d6dcc4b2c94a2695307
Lekewhite01 <lekewhite@outlook.com>
2024-03-07 23:40:25 +0100

    cleaned up modules

--- app/utils/data_preprocessing.py
+++ app/utils/data_preprocessing.py
@@ -1,17 +1,6 @@
 import re
 import pandas as pd
 from load_data import fetch_job_listings, convert_to_dataframe
-# utils/data_preprocessing.py
-
-# def preprocess_user_preferences(user_preferences):
-#     """
-#     Preprocess the user preferences before they are fed into the machine learning model.
-#     This is just a placeholder function - you'll need to adapt it based on your model's requirements.
-#     """
-#     # Example: Convert the preferences string to a format your model expects
-#     # This could be more complex depending on your model's needs
-#     processed_preferences = user_preferences.lower()
-#     return processed_preferences
 
 def preprocess_data():
     """
--- app/utils/model_loading.py
+++ app/utils/model_loading.py
@@ -1,18 +1,4 @@
-# utils/model_loading.py
-
-# import joblib
 import pickle
-
-# def load_recommendation_model(model_path):
-#     """
-#     Load the machine learning model for job recommendations.
-#     """
-#     try:
-#         model = joblib.load(model_path)
-#         return model
-#     except Exception as e:
-#         print(f"Error loading the model: {e}")
-#         return None
     
 def load_recommendation_model(filename):
     """
