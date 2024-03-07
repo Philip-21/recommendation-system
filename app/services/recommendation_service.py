32042d1554ac69b9592895c20539f162b666ab7c
Lekewhite01 <lekewhite@outlook.com>
2024-03-07 23:42:26 +0100

    commented out previous recommendation logic

--- app/services/recommendation_service.py
+++ app/services/recommendation_service.py
@@ -16,6 +16,33 @@ else:
     print("Model file not found. Please train and save the model first.")
 
 
+# def get_recommendations(user_id):
+#     user = User.query.get(user_id)
+#     if not user:
+#         return []
+
+#     # Preprocess user preferences
+#     user_preferences = preprocess_user_preferences(user.job_preferences)
+
+#     # Use the machine learning model to generate recommendations
+#     try:
+#         recommended_jobs = model.predict([user_preferences])
+#     except Exception as e:
+#         print(f"Error during model prediction: {e}")
+#         return []
+
+#     recommendations = []
+#     for job_id in recommended_jobs:
+#         job = Job.query.get(job_id)
+#         if job:
+#             recommendations.append({
+#                 'job_id': job.job_id,
+#                 'title': job.title,
+#                 'description': job.description,
+#             })
+
+#     return recommendations
+
 def get_recommendations(job_title, df, model, top_n=5):
     """
     Generates recommendations based on a given job title.
